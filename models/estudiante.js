787ab6f9d04f56644b4786ebbfc86c0bedaaef25
lisnarv <valeralisnar30@gmail.com>
2025-03-16 12:52:21 +0100

    creando la clase

---
+++ models/estudiante.js
@@ -0,0 +1,11 @@
+class Estudiante {
+    constructor(id, nombre, edad, carrera) {
+      this.id = id;
+      this.nombre = nombre;
+      this.edad = edad;
+      this.carrera = carrera;
+    }
+  }
+  
+  module.exports = Estudiante;
+  
